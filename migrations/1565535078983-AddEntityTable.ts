b89e9e4c48b986645798eb6703a10f9dbd056e55
GabrielGhimici <gabriel.ghimici@teads.tv>
2019-08-11 17:55:39 +0300

    Added some migrations

---
+++ migrations/1565535078983-AddEntityTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddEntityTable1565535078983 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
