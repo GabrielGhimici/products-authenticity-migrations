ee1dbcffff33959ef38eba3044dc302cb8a0a2b1
GabrielGhimici <gabriel.ghimici@teads.tv>
2019-08-11 23:21:50 +0300

    Add users

--- migrations/prod/1565553387468-AddRoles.ts
+++ migrations/prod/1565553387468-AddRoles.ts
@@ -4,13 +4,17 @@ export class AddRoles1565553387468 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
       await queryRunner.query(
-        ``
+        `insert into role (name, status)
+         values ('User', 'enabled'),
+                ('Organization user', 'enabled'),
+                ('Organization admin', 'enabled'),
+                ('God', 'enabled')`
       );
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
       await queryRunner.query(
-        ``
+        `delete from role`
       );
     }
 
